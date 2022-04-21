c7391936e8b78bad08db75f6e9baf14900a5dca0
eme22 <48258734+eme22@users.noreply.github.com>
2022-04-21 13:23:24 -0500

    Url updated from https://waifu.pics/api/ to https://api.waifu.pics/

--- src/main/java/pics/waifu/Constants.java
+++ src/main/java/pics/waifu/Constants.java
@@ -1,5 +1,5 @@
 package pics.waifu;
 
 public class Constants {
-    public static final String BaseURL = "https://waifu.pics/api";
+    public static final String BaseURL = "https://api.waifu.pics/";
 }
